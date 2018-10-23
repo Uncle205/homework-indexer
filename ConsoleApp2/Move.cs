0ce89fee000de88aa1c7f8c5f745c557e153913b
амир абдрахманов <Uncle205@mail.ru>
2018-10-23 20:15:32 +0600

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Move.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+   
+
+    class Move
+    {
+        int[] mas = { 1, 2, 3, 4, 5 };
+    public int this[int index]
+    {
+        get { return mas[index] * 2; }
+        set { mas[index] = value; }
+    }}
+    
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        int[] mas = { 1, 2, 3, 4, 5 };
+        static void Main(string[] args)
+        {
+            Move move= new Move();
+            Console.WriteLine( move[2]);
+    }
+        
+    }
+}
